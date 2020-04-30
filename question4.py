92e0dc4402c3740418e6ee947bb560c13c863133
kebron88 <kebron88>
2020-04-30 17:24:20 +0900

    completed assignment

--- question4.py
+++ question4.py
@@ -7,9 +7,15 @@ sum of the squares of the elements. So the cosine similarity of arrays arr1 and 
 Round your answer to 4 decimal places. Suppose arr1 = array([1,2,3]) and arr2 = array([-4,0,3.5]). Then f(arr1, arr2) should return 0.3268
 """
 
-def f(arr1, arr2):
-    ##########YOUR CODE HERE##########
-    pass
+import numpy as np
+arr1 = np.array([2,6,8])
+arr2 = np.array([-4,5,-2.5])
+
+def f(arr1,arr2):
+    a= arr1/np.linalg.norm(arr1)
+    b=arr2/np.linalg.norm(arr2)
+    return round(np.dot(a,b),4)
+print(f(arr1,arr2))
     ###########END CODE###############
 
 if __name__=='__main__':
